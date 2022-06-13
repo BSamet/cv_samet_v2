25904130a925298d4e072b24fe3949607b15a122
samet <samet.sb.68@gmail.com>
2022-06-13 16:41:03 +0200

    Samet : Navbar finished

--- cvsamet_front/src/common/components/elements/navbar/navbar.tsx
+++ cvsamet_front/src/common/components/elements/navbar/navbar.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "../../../styles/Home.module.scss";
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Link from '@mui/material/Link';
 
 const Navbar = () => {
+    const [navLocation, setNavLocation] = useState('');
+
+    const navbarData = [
+        {
+            title: "Accueil"
+        },
+        {
+            title: "Compétences professionnelles"
+        },
+        {
+            title: "Portfolio"
+        },
+        {
+            title: "Centres d'intérêt"
+        },
+        {
+            title: "Contact"
+        },
+    ]
+
+    function setNavigation (title: string) {
+        setNavLocation(title)
+    }
+
     return (
         <AppBar
             position="sticky"
@@ -13,36 +37,18 @@ const Navbar = () => {
         >
             <Toolbar>
                 <nav className={styles.navbar__nav}>
-                    <Link
-                        href="#"
-                        className={styles.navbar__button}
-                    >
-                        Accueil
-                    </Link>
-                    <Link
-                        href="#"
-                        className={styles.navbar__button}
-                    >
-                        Compétences professionnelles
-                    </Link>
-                    <Link
-                        href="#"
-                        className={styles.navbar__button}
-                    >
-                        Portfolio
-                    </Link>
-                    <Link
-                        href="#"
-                        className={styles.navbar__button}
-                    >
-                        Centres d'intérêt
-                    </Link>
-                    <Link
-                        href="#"
-                        className={styles.navbar__button}
-                    >
-                        Contact
-                    </Link>
+                    {navbarData.map((nav, index) => {
+                        return (
+                            <Link
+                                key={index}
+                                className={styles.navbar__button}
+                                onClick={() => setNavigation(nav.title)}
+                                sx={nav.title === navLocation ? {color: "#ffffff"} : {color: "#999999"}}
+                            >
+                                {nav.title}
+                            </Link>
+                        )
+                    })}
                 </nav>
             </Toolbar>
         </AppBar>
