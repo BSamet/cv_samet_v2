e206cb1bbcb1fd9d440e3fb3a67f939ce7c61650
Samet <samet.sb.68@gmail.com>
2022-07-26 15:34:36 +0200

    Samet : Front - ability section is done

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DevSkills from "../common/components/elements/devSkills/devSkills";
+import Ability from "../common/components/elements/ability/ability";
 
 const Skills = () => {
     return (
         <section>
             <DevSkills />
+            <Ability />
         </section>
     );
 };
